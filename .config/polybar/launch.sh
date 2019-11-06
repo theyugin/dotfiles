199f2cb5daf978de2f49173e9b556ae7ade52d09
TheyuGin <theyugin@gmail.com>
2019-11-06 20:10:48 +0500

    redid polybar monitor stuff

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,8 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.01; done
 
+# Set proper MONITOR variable
+MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g') polybar main &
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar main &
 polybar secondary &
 
 echo "Polybar launched..."
